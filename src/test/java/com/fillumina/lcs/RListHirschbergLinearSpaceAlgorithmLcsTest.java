80cb230b24667e55b7b112a1a632778167b30110
Francesco Illuminati <fillumina@gmail.com>
2015-10-21 13:13:18 +0200

    fixed test

--- src/test/java/com/fillumina/lcs/RListHirschbergLinearSpaceAlgorithmLcsTest.java
+++ src/test/java/com/fillumina/lcs/RListHirschbergLinearSpaceAlgorithmLcsTest.java
@@ -8,28 +8,25 @@ import static org.junit.Assert.*;
  *
  * @author Francesco Illuminati <fillumina@gmail.com>
  */
-public class RListHirschbergLinearSpaceAlgorithmLcsTest {
+public class RListHirschbergLinearSpaceAlgorithmLcsTest extends AbstractLcsTest {
+
+    @Override
+    protected Lcs<Character> getLcsAlgorithm() {
+        return new RListHirschbergLinearSpaceAlgorithmLcs<Character>() {
+
+            @Override
+            public List<Character> lcs(List<Character> xs,
+                    List<Character> ys) {
+                count(xs, ys);
+                return super.lcs(xs, ys);
+            }
+        };
+    }
 
     @Test
     public void shouldGetTheRightResult() {
-
-        new AbstractLcsTestExecutor() {
-
-            @Override
-            protected Lcs<Character> getLcsAlgorithm() {
-                return new RListHirschbergLinearSpaceAlgorithmLcs<Character>() {
-
-                    @Override
-                    public List<Character> lcs(List<Character> xs,
-                            List<Character> ys) {
-                        count(xs, ys);
-                        return super.lcs(xs, ys);
-                    }
-                };
-            }
-
-        }.lcs("PONY", "PYTHON")
-                .assertResult("PON")
-                .printerr();
+        lcs("PONY", "PYTHON")
+            .assertResult("PON");
+//            .printerr();
     }
 }
