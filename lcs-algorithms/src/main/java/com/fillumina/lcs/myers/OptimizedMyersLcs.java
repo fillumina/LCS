42693910a38a19c19c6801db60c8105e65992c3b
Francesco Illuminati <fillumina@gmail.com>
2015-12-11 17:36:29 +0100

    defining variable closer to where it's used

--- lcs-algorithms/src/main/java/com/fillumina/lcs/myers/OptimizedMyersLcs.java
+++ lcs-algorithms/src/main/java/com/fillumina/lcs/myers/OptimizedMyersLcs.java
@@ -31,14 +31,12 @@ public class OptimizedMyersLcs implements Lcs {
         int m = b.length;
         int max = n + m + 1;
 
-        int size = (max << 1) + 1;
         int[][] vv = new int[max][];
-        int[] v = new int[size];
+        int[] v = new int[(max << 1) + 1];
 
-        int maxk, next, prev, x=-1, y, d, k=-1, s;
+        int size, maxk, next, prev, x=-1, y, d, k=-1;
         FILL_THE_TABLE:
         for (d = 0; d < max; d++) {
-            size = (d<<1) + 3;
             for (k = -d; k <= d; k += 2) {
                 maxk = max + k;
                 next = v[maxk + 1]; // down
@@ -55,11 +53,13 @@ public class OptimizedMyersLcs implements Lcs {
                 }
                 v[maxk] = x;
                 if (x >= n && y >= m) {
+                    size = (d<<1) + 3;
                     vv[d] = new int[size];
                     System.arraycopy(v, max-d-1, vv[d], 0, size);
                     break FILL_THE_TABLE;
                 }
             }
+            size = (d<<1) + 3;
             vv[d] = new int[size];
             System.arraycopy(v, max-d-1, vv[d], 0, size);
         }
@@ -85,7 +85,7 @@ public class OptimizedMyersLcs implements Lcs {
             }
 
             if (x != xMid) {
-                for (s=x-1; s>=xMid; s--) {
+                for (int s=x-1; s>=xMid; s--) {
                     index--;
                     result[index] = a[s];
                 }
